724b09c142357c418349ae3fe2bf8dee0b004195
Rostislav Pasechnikov <pasechnikov_ros@hotmail.com>
2020-10-09 21:57:05 +1100

    Basic tetris board build + pixels styled

--- src/game-board.ts
+++ src/game-board.ts
@@ -1,10 +1,20 @@
 export class GameBoard {
+  private readonly _boardHeight = 20;
+  private readonly _boardWidth = 10;
+  private _gameBoard: Element;
+
   init(): void {
+    this._gameBoard = document.getElementsByClassName('game-board')[0];
+
     this.render();
   }
 
   private render(): void {
-    const gameBoard = document.getElementsByClassName('game-board')[0];
-    console.log(gameBoard);
+    for (var i = 0; i < this._boardHeight * this._boardWidth; i++) {
+      var cell = document.createElement('div');
+      cell.className = 'cell';
+
+      this._gameBoard.appendChild(cell);
+    }
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,3 @@
 import { GameBoard } from './game-board';
 
-function main(): void {
-  console.log('Main!');
-
-  // Test
-  const gameBoard = new GameBoard();
-  gameBoard.init();
-}
-
-main();
+new GameBoard().init();
