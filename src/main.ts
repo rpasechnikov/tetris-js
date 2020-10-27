623af2ea56bd3b0462d3f1e6c4f3b98278b93305
Rostislav Pasechnikov <pasechnikov_ros@hotmail.com>
2020-10-27 21:48:02 +1100

    WIP score based speed

--- src/main.ts
+++ src/main.ts
@@ -6,24 +6,35 @@ import { Controls } from './types/controls';
 main();
 
 function main(): void {
-  const timestepMs = BOUNDS.TIMESTEP_MS;
+  const timestepReduction = 10;
   const gameControls = new Controls();
   const gameBoard = new Board();
+
+  let timestepMs = BOUNDS.TIMESTEP_MS;
 
   gameControls.init();
   gameBoard.init();
 
   gameControls.playPause$.subscribe(play => gameBoard.playPause(play));
-  gameControls.reset$.subscribe(_ => gameBoard.reset());
+  gameControls.reset$.subscribe(_ => {
+    gameBoard.reset();
+    timestepMs = BOUNDS.TIMESTEP_MS;
+  });
 
-  gameBoard.rowResolved$.subscribe(_ => gameControls.incrementScore());
+  gameBoard.rowResolved$.subscribe(_ => {
+    gameControls.incrementScore();
+
+    // TODO: update timestep speed. Probably using setTimeout()
+    // timestepMs -= timestepReduction;
+  });
+
   gameBoard.gameOver$.subscribe(_ => gameControls.displayGameOverMessage());
 
-  setInterval(doTimestep, timestepMs, [gameControls, gameBoard]);
+  setInterval(doTimestep, timestepMs, [gameControls, gameBoard], timestepMs);
 }
 
-function doTimestep(updatableControls: Updatable[]): void {
-  console.log('Timestep...');
+function doTimestep(updatableControls: Updatable[], timestepMs: number): void {
+  console.log(`Timestep...`);
 
   for (const control of updatableControls) {
     control.update();
